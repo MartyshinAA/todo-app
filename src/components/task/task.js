98400ab4b631a18ef5119ee9d00f285966605e27
Anton M <MartyshinAA@gmail.com>
2022-07-07 18:20:11 +0700

    minor fix propTypes

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,80 +1,91 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 import './task.css';
 
 export default class Task extends Component {
-
   static defaultProps = {
     description: 'New Task',
     completed: false,
     editing: false,
     created: 'created some time ago',
-  }
-  
+  };
+
   static propsTypes = {
     description: PropTypes.string,
-    completed: PropTypes.boolean,
-    editing: PropTypes.boolean,
+    completed: PropTypes.bool,
+    editing: PropTypes.bool,
     created: PropTypes.string,
     onDescriptionCompleted: PropTypes.func.isRequired,
     onDescriptionEditing: PropTypes.func.isRequired,
     deleteTodoElement: PropTypes.func.isRequired,
-  }
+  };
 
   state = {
-    description: this.props.description
-  }
-  
-  onSubmitEditing = e => {
-    e.preventDefault();
-    this.props.onDescriptionEditing(this.state.description); 
-  }
+    description: this.props.description,
+  };
 
-  editTodoElement = e => {
-    this.props.editTodoElement(e)
-  }
+  onSubmitEditing = (e) => {
+    e.preventDefault();
+    this.props.onDescriptionEditing(this.state.description);
+  };
+
+  editTodoElement = (e) => {
+    this.props.editTodoElement(e);
+  };
 
   render() {
-    const { description, completed, editing, created, onDescriptionCompleted, onDescriptionEditing, deleteTodoElement } = this.props;
+    const {
+      description,
+      completed,
+      editing,
+      created,
+      onDescriptionCompleted,
+      onDescriptionEditing,
+      deleteTodoElement,
+    } = this.props;
 
     let classNames = 'task';
     if (completed) {
-      classNames +=` completed`;
+      classNames += ` completed`;
     }
     if (editing) {
-      classNames +=` editing`;
+      classNames += ` editing`;
     }
 
     return (
-        <li className={ classNames }>
-            <div className="view">
-              <input 
-                className="toggle" 
-                type="checkbox" 
-                checked= { completed }
-                onChange = { onDescriptionCompleted }
-                />
-              <label>
-                <span 
-                className = "description"
-                >{ description }
-                </span>
-                <span className="created">{ created }</span>
-              </label>
-              <button className="icon icon-edit"
-                      onClick = { onDescriptionEditing }></button>
-              <button className="icon icon-destroy"
-              onClick={ deleteTodoElement }></button>
-            </div>
-            {editing && <form onSubmit = { this.onSubmitEditing }>
-                          <input type="text" 
-                                  className="edit" 
-                                  // В задании не было реализовать функцию редактирования, но для учебного проекта пусть будет. Впредь подобного обязуюсь не совершать.
-                                  onChange = { this.editTodoElement }
-                                  value={ description }
-                                  ></input>
-                        </form>} 
-          </li>
-    )
+      <li className={classNames}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={completed}
+            onChange={onDescriptionCompleted}
+          />
+          <label>
+            <span className="description">{description}</span>
+            <span className="created">{created}</span>
+          </label>
+          <button
+            className="icon icon-edit"
+            onClick={onDescriptionEditing}
+          ></button>
+          <button
+            className="icon icon-destroy"
+            onClick={deleteTodoElement}
+          ></button>
+        </div>
+        {editing && (
+          <form onSubmit={this.onSubmitEditing}>
+            <input
+              type="text"
+              className="edit"
+              // В задании не было реализовать функцию редактирования, но для учебного проекта пусть будет. Впредь подобного обязуюсь не совершать.
+              onChange={this.editTodoElement}
+              value={description}
+            ></input>
+          </form>
+        )}
+      </li>
+    );
   }
-};
+}
